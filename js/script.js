27a0cd238736c4d3524e37e7b6ead93120c64b22
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-20 19:31:22 +0530

    Canvas Setup working and filters are working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const filters = {
+let filters = {
     brightness: {
         unit: "%",
         value: 100,
@@ -11,13 +11,7 @@ const filters = {
         min: 0,
         max: 200,
     },
-    exposure: {
-        unit: "%",
-        value: 100,
-        min: 0,
-        max: 200,
-    },
-    saturation: {
+    saturate: {
         unit: "%",
         value: 100,
         min: 0,
@@ -61,9 +55,19 @@ const filters = {
     },
 }
 
+const imageCanvas = document.querySelector("#image-canvas");
+const imageInput = document.querySelector("#image-input");
+const canvasCtx = imageCanvas.getContext("2d");
+const resetBtn = document.querySelector("#reset-btn");
+const downloadBtn = document.querySelector("#download-btn");
+
+let file = null;
+let image = null;
+let fileName = null;
+
 const filtersContainer = document.querySelector(".filters");
 
-function createFilterElement(name, unit="%", value, min, max){
+function createFilterElement(name, unit = "%", value, min, max) {
     const div = document.createElement("div");
     div.classList.add("filter");
 
@@ -75,18 +79,63 @@ function createFilterElement(name, unit="%", value, min, max){
     input.id = name;
 
     const p = document.createElement("p");
-    // p.innerText = `${name.charAt(0).toUpperCase() + name.slice(1)}: ${value}${unit}`;
-    p.innerText = name
+    p.innerText = `${name.charAt(0).toUpperCase() + name.slice(1)}: ${value}${unit}`;
 
     div.appendChild(p);
     div.appendChild(input);
 
+    input.addEventListener("input", (event) => {
+        filters[name].value = input.value;
+        console.log(filters[name])
+        p.innerText = `${name.charAt(0).toUpperCase() + name.slice(1)}: ${filters[name].value}${unit}`;
+
+        applyFilters();
+    });
+
     return div;
 }
 
-Object.keys(filters).forEach(filterName => {
-    let filterElement = createFilterElement(filterName, filters[filterName].unit, filters[filterName].value, filters[filterName].min, filters[filterName].max);
+function createFilters() {
+    Object.keys(filters).forEach(filterName => {
+        let filterElement = createFilterElement(filterName, filters[filterName].unit, filters[filterName].value, filters[filterName].min, filters[filterName].max);
 
-    filtersContainer.appendChild(filterElement);
+        filtersContainer.appendChild(filterElement);
+    })
+}
+
+createFilters();
+
+imageInput.addEventListener("change", (e) => {
+    file = e.target.files[0];
+    fileName = file.name;
+    const placeholder = document.querySelector(".placeholder");
+    imageCanvas.style.display = "block";
+    placeholder.style.display = "none";
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+        image = img;
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+        canvasCtx.drawImage(img, 0, 0);
+    }
 })
 
+function applyFilters() {
+    if (!image) return;
+    canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+
+    canvasCtx.filter = `
+    brightness(${filters.brightness.value}${filters.brightness.unit})
+    contrast(${filters.contrast.value}${filters.contrast.unit})
+    saturate(${filters.saturate.value}${filters.saturate.unit})
+    hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit})
+    blur(${filters.blur.value}${filters.blur.unit})
+    grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+    sepia(${filters.sepia.value}${filters.sepia.unit})
+    opacity(${filters.opacity.value}${filters.opacity.unit})
+    invert(${filters.invert.value}${filters.invert.unit})`.trim();
+
+    canvasCtx.drawImage(image, 0, 0);
+}
