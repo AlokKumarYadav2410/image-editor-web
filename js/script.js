dc41e9e0e57bf906ad18de6feefbde6aa8539d2c
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-20 19:33:25 +0530

    Implemented filters reset functionality

--- js/script.js
+++ js/script.js
@@ -139,3 +139,66 @@ function applyFilters() {
 
     canvasCtx.drawImage(image, 0, 0);
 }
+
+resetBtn.addEventListener("click", () => {
+    filters = {
+        brightness: {
+            unit: "%",
+            value: 100,
+            min: 0,
+            max: 200,
+        },
+        contrast: {
+            unit: "%",
+            value: 100,
+            min: 0,
+            max: 200,
+        },
+        saturate: {
+            unit: "%",
+            value: 100,
+            min: 0,
+            max: 200,
+        },
+        hueRotation: {
+            unit: "deg",
+            value: 0,
+            min: 0,
+            max: 360,
+        },
+        blur: {
+            unit: "px",
+            value: 0,
+            min: 0,
+            max: 20,
+        },
+        grayscale: {
+            unit: "%",
+            value: 0,
+            min: 0,
+            max: 100,
+        },
+        sepia: {
+            unit: "%",
+            value: 0,
+            min: 0,
+            max: 100,
+        },
+        opacity: {
+            unit: "%",
+            value: 100,
+            min: 0,
+            max: 100,
+        },
+        invert: {
+            unit: "%",
+            value: 0,
+            min: 0,
+            max: 100,
+        },
+    };
+
+    applyFilters();
+    filtersContainer.innerHTML = "";
+    createFilters();
+});
